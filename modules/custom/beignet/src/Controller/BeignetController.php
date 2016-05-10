8bb5cca7147fb0a36af9bf42189127d77174959c
Ryan Weaver <ryan@thatsquality.com>
2016-05-10 11:31:45 -0500

    Service: use controller create for configFactory

--- modules/custom/beignet/src/Controller/BeignetController.php
+++ modules/custom/beignet/src/Controller/BeignetController.php
@@ -2,10 +2,30 @@
 
 namespace Drupal\beignet\Controller;
 
-class BeignetController
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class BeignetController implements ContainerInjectionInterface
 {
+    private $configFactory;
+
+    public function __construct($configFactory)
+    {
+        $this->configFactory = $configFactory;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        return new self($container->get('config.factory'));
+    }
+
     public function eatBeignets($number)
     {
+        if ($number === null) {
+            $config = $this->configFactory->get('beignet.settings');
+            $number = $config->get('default_number');
+        }
+
         return [
             '#type' => 'markup',
             '#markup' => sprintf('NOM %s beignets', $number),
