8b4c0b54249af0054cd5efc12cfa111e877baeed
Ryan Weaver <ryan@thatsquality.com>
2016-05-10 11:31:55 -0500

    Event: Log oven duration

--- modules/custom/beignet/src/EventSubscriber/OvenInitializer.php
+++ modules/custom/beignet/src/EventSubscriber/OvenInitializer.php
@@ -3,24 +3,34 @@
 namespace Drupal\beignet\EventSubscriber;
 
 use Drupal\beignet\Service\BeignetBakery;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class OvenInitializer implements EventSubscriberInterface {
   private $bakery;
+  private $ovenStartedAt;
+  private $loggerChannelFactory;
 
-  public function __construct(BeignetBakery $bakery)
+  public function __construct(BeignetBakery $bakery, LoggerChannelFactoryInterface $loggerChannelFactory)
   {
     $this->bakery = $bakery;
+    $this->loggerChannelFactory = $loggerChannelFactory;
   }
 
   public function onKernelRequest()
   {
     $this->bakery->turnOnOven();
+    $this->ovenStartedAt = microtime(true);
   }
 
   public function onKernelResponse()
   {
     $this->bakery->turnOffOven();
+    $duration = microtime(true) - $this->ovenStartedAt;
+
+    $this->loggerChannelFactory
+      ->get('database')
+      ->info(sprintf('Oven was on for %s seconds', $duration));
   }
 
   static function getSubscribedEvents() {
